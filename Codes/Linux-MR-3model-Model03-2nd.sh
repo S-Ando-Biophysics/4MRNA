fa090bc9533bdab5d3c2a7891e85f111e80ee298
Shin Ando <shinando.forwork@gmail.com>
2025-08-21 17:44:48 +0900

    Add files via upload

---
+++ Codes/Linux-MR-3model-Model03-2nd.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+parent_directory=""
+shopt -s nullglob
+mtz_candidates=( "${parent_directory}"/*.mtz )
+shopt -u nullglob
+if (( ${#mtz_candidates[@]} == 0 )); then echo "Error: No .mtz file found in ${parent_directory}" >&2; exit 1; elif (( ${#mtz_candidates[@]} == 1 )); then mtz_file="${mtz_candidates[0]}"; else mtz_file="$(ls -t "${parent_directory}"/*.mtz | head -n1)"; echo "Warning: Multiple .mtz files found. Using the most recent: ${mtz_file}" >&2; fi
+mkdir -p "${parent_directory}/MR-Model03-2nd"
+file_path="${parent_directory}/MR-Model03-2nd"
+cd "${file_path}"
+pdb_directory_1="${file_path}/Model01i"
+pdb_directory_2="${file_path}/Model02i"
+pdb_directory_3="${file_path}/Model03"
+finish_directory="${pdb_directory_3}/finish"
+mkdir -p "${pdb_directory_1}"
+mkdir -p "${pdb_directory_2}"
+mkdir -p "${pdb_directory_3}"
+mkdir -p "${finish_directory}"
+cp "${parent_directory}/Model03-2"/*.pdb "${pdb_directory_3}"
+cp "${parent_directory}/Model01-2"/Model01best.pdb "${pdb_directory_1}"
+cp "${parent_directory}/Model02-2"/Model02best.pdb "${pdb_directory_2}"
+for pdb_file_1 in "${pdb_directory_1}"/*.pdb; do
+  for pdb_file_2 in "${pdb_directory_2}"/*.pdb; do
+    for pdb_file_3 in "${pdb_directory_3}"/*.pdb; do
+      base_name_1=$(basename "${pdb_file_1}" .pdb)
+      base_name_2=$(basename "${pdb_file_2}" .pdb)
+      base_name_3=$(basename "${pdb_file_3}" .pdb)
+      output_directory="phaser-${base_name_1}-${base_name_2}-${base_name_3}"
+      mkdir -p "${output_directory}"
+      phaser <<EOF
+TITLe ${base_name_1}-${base_name_2}-${base_name_3}
+MODE MR_AUTO
+HKLIn ${mtz_file}
+ENSEmble ${base_name_1} PDB ${pdb_file_1} IDENtity 80
+ENSEmble ${base_name_2} PDB ${pdb_file_2} IDENtity 80
+ENSEmble ${base_name_3} PDB ${pdb_file_3} IDENtity 80
+COMPosition NUCLeic MW 1000 NUM 1
+COMPosition NUCLeic MW 2000 NUM 2
+COMPosition NUCLeic MW 3000 NUM 3
+SEARch ENSEmble ${base_name_1} NUM 1
+SEARch ENSEmble ${base_name_2} NUM 2
+SEARch ENSEmble ${base_name_3} NUM 3
+EOF
+      mv PHASER.sol PHASER.1.mtz PHASER.1.pdb "${output_directory}/"
+      cp "${pdb_file_1}" "${finish_directory}/"
+    done
+  done
+done
+llg_extracted="./extracted_data_LGG.txt"
+llg_output_file="./TopLLG.txt"
+: > "$llg_extracted"; : > "$llg_output_file"
+for folder_path in ./phaser-*/; do
+  phaser_sol="${folder_path}PHASER.sol"
+  [ -f "$phaser_sol" ] || continue
+  folder_name=$(awk 'NR==1{print $2; exit}' "$phaser_sol")
+  solu_set_line=$(awk '/SOLU SET/ {print; exit}' "$phaser_sol")
+  [ -n "$solu_set_line" ] || continue
+  result=$(echo "$solu_set_line" | awk '{sep=""; for(i=1;i<=NF;i++){ if($i ~ /LLG=/){ gsub(/[^0-9.]/,"",$i); printf "%s%s", sep, $i; sep=","; }} printf "\n"}')
+  [ -n "$result" ] || continue
+  printf '\n%s\n%s\n' "$folder_name" "$result" >> "$llg_extracted"
+  max_value=$(echo "$result" | awk -F',' '{max=$1; for(i=2;i<=NF;i++) if($i>max) max=$i; print max}')
+  printf '\n%s,%s\n' "$folder_name" "$max_value" >> "$llg_output_file"
+done
+tfz_extracted="./extracted_data_TFZ.txt"
+tfz_output_file="./TopTFZ.txt"
+: > "$tfz_extracted"; : > "$tfz_output_file"
+for folder_path in ./phaser-*/; do
+  phaser_sol="${folder_path}PHASER.sol"
+  [ -f "$phaser_sol" ] || continue
+  folder_name=$(awk 'NR==1{print $2; exit}' "$phaser_sol")
+  solu_set_line=$(awk '/SOLU SET/ {print; exit}' "$phaser_sol")
+  [ -n "$solu_set_line" ] || continue
+  result=$(echo "$solu_set_line" | awk '{sep=""; for(i=1;i<=NF;i++){ if($i ~ /TFZ=/){ gsub(/[^0-9.]/,"",$i); printf "%s%s", sep, $i; sep=","; }} printf "\n"}')
+  [ -n "$result" ] || continue
+  printf '\n%s\n%s\n' "$folder_name" "$result" >> "$tfz_extracted"
+  max_value=$(echo "$result" | awk -F',' '{max=$1; for(i=2;i<=NF;i++) if($i>max) max=$i; print max}')
+  printf '\n%s,%s\n' "$folder_name" "$max_value" >> "$tfz_output_file"
+done
+{ printf 'Model TopLLG TopTFZ\n'; awk -F',' 'NR==FNR{llg[$1]=$2; next} {print $1, llg[$1], $2}' OFS=' ' TopLLG.txt TopTFZ.txt; } > "${file_path}/results.txt"
