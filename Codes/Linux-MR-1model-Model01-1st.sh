cb2e90a5cca38e7b6b25227ac90374c1680ab7dc
Shin Ando <shinando.forwork@gmail.com>
2025-08-21 16:15:30 +0900

    Update Linux-MR-1model-Model01-1st.sh

--- Codes/Linux-MR-1model-Model01-1st.sh
+++ Codes/Linux-MR-1model-Model01-1st.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-
 parent_directory=""
-
 mtz_files=( "${parent_directory}"/*.mtz )
 if [ ${#mtz_files[@]} -eq 0 ]; then
   echo "Error: No .mtz file found in ${parent_directory}" >&2
@@ -12,24 +10,20 @@ elif [ ${#mtz_files[@]} -gt 1 ]; then
   exit 1
 fi
 mtz_file="${mtz_files[0]}"
-
 mkdir -p "${parent_directory}/MR-Model01-1st"
 file_path="${parent_directory}/MR-Model01-1st"
 cd "${file_path}" || exit 1
-
 pdb_directory_1="${file_path}/Model01"
 finish_directory="${pdb_directory_1}/finish"
 mkdir -p "${pdb_directory_1}" "${finish_directory}"
-
 cp "${parent_directory}/Model01-1"/*.pdb "${pdb_directory_1}" 2>/dev/null || true
 rm -f "${pdb_directory_1}"/??????.pdb
-
 for pdb_file_1 in "${pdb_directory_1}"/*.pdb; do
   [ -f "${pdb_file_1}" ] || continue
   base_name_1=$(basename "${pdb_file_1}" .pdb)
   output_directory="phaser-${base_name_1}"
   mkdir -p "${output_directory}"
-  phaser <<EOF 2>&1 | tee "${output_directory}/phaser.log"
+  if ! phaser <<EOF
 TITLe ${base_name_1}
 MODE MR_AUTO
 HKLIn ${mtz_file}
@@ -42,11 +36,9 @@ EOF
   [ -f PHASER.1.pdb ] && mv PHASER.1.pdb "${output_directory}/"
   cp "${pdb_file_1}" "${finish_directory}/"
 done
-
 llg_extracted="./extracted_data_LGG.txt"
 llg_output_file="./TopLLG.txt"
 : > "$llg_extracted"; : > "$llg_output_file"
-
 for folder_path in ./phaser-*/; do
   [ -d "$folder_path" ] || continue
   phaser_sol="${folder_path}PHASER.sol"
@@ -70,11 +62,9 @@ for folder_path in ./phaser-*/; do
   max_value=$(echo "$result" | awk -F',' '{max=$1; for(i=2;i<=NF;i++) if($i>max) max=$i; print max}')
   printf '\n%s,%s\n' "$folder_name" "$max_value" >> "$llg_output_file"
 done
-
 tfz_extracted="./extracted_data_TFZ.txt"
 tfz_output_file="./TopTFZ.txt"
 : > "$tfz_extracted"; : > "$tfz_output_file"
-
 for folder_path in ./phaser-*/; do
   [ -d "$folder_path" ] || continue
   phaser_sol="${folder_path}PHASER.sol"
@@ -98,17 +88,14 @@ for folder_path in ./phaser-*/; do
   max_value=$(echo "$result" | awk -F',' '{max=$1; for(i=2;i<=NF;i++) if($i>max) max=$i; print max}')
   printf '\n%s,%s\n' "$folder_name" "$max_value" >> "$tfz_output_file"
 done
-
 {
   printf 'Model TopLLG TopTFZ\n'
   awk -F',' 'NR==FNR{llg[$1]=$2; next} {print $1, llg[$1], $2}' OFS=' ' TopLLG.txt TopTFZ.txt
 } > "${file_path}/results.txt"
-
 mkdir -p "${parent_directory}/Model01-2"
 mr_dir="${parent_directory}/MR-Model01-1st"
 model_dir_1="${parent_directory}/Model01-1"
 model_dir_2="${parent_directory}/Model01-2"
-
 shopt -s nullglob
 three_dna_candidates=( "${model_dir_1}"/3DNA-??NA )
 if [ ${#three_dna_candidates[@]} -ge 1 ]; then
@@ -118,13 +105,10 @@ if [ ${#three_dna_candidates[@]} -ge 1 ]; then
   fi
 fi
 shopt -u nullglob
-
 grep -F 'TILT'  "${mr_dir}/results.txt" > "${mr_dir}/results-tilt.txt"  || :
 grep -F 'ROLL'  "${mr_dir}/results.txt" > "${mr_dir}/results-roll.txt"  || :
 grep -F 'TWIST' "${mr_dir}/results.txt" > "${mr_dir}/results-twist.txt" || :
-
 : > "${mr_dir}/results-good-num.txt"
-
 process_category () {
   local KEYWORD="$1"
   local INFILE="$2"
@@ -138,7 +122,6 @@ process_category () {
     }' "$INFILE" \
   | sort -nr -k1,1 -k2,2 | head -2 \
   | awk -v pfx="$PREFIX" '{print "bp_step_" pfx $3}' >> "${mr_dir}/results-good-num.txt"
-
   awk -v kw="$KEYWORD" '
     {
       s=$1; n1=$2+0; n2=$3+0;
@@ -147,7 +130,6 @@ process_category () {
     }' "$INFILE" \
   | sort -nr -k1,1 -k2,2 | head -2 \
   | awk -v pfx="$PREFIX" '{print "bp_step_" pfx $3}' >> "${mr_dir}/results-good-num.txt"
-
   awk -v kw="$KEYWORD" '
     {
       s=$1; n1=$2+0; n2=$3+0;
@@ -160,11 +142,9 @@ process_category () {
 
   awk '!seen[$0]++' "${mr_dir}/results-good-num.txt" > "${mr_dir}/.results-good-num.tmp" && mv "${mr_dir}/.results-good-num.tmp" "${mr_dir}/results-good-num.txt"
 }
-
 process_category "TILT"  "${mr_dir}/results-tilt.txt"  "Tilt"
 process_category "ROLL"  "${mr_dir}/results-roll.txt"  "Roll"
 process_category "TWIST" "${mr_dir}/results-twist.txt" "Twist"
-
 if [ -s "${mr_dir}/results-good-num.txt" ]; then
   shopt -s nullglob
   three_dna_dirs=( "${model_dir_1}"/3DNA-??NA )
